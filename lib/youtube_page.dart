a9adad3a8aef03b841aa9b30b1aa5c0ccdee0fb3
rushikeshdaye110 <rushikeshdaye2004@gmail.com>
2023-05-30 15:00:23 -0700

    youtube

---
+++ lib/youtube_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class youtube_page extends StatelessWidget {
+  const youtube_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        
+      ),
+      drawer: Drawer(backgroundColor: Colors.black12),
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
